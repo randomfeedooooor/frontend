e7315a474d7c53ea7f4b33356bf5dbfe8c6dae4f
Rootul Patel <rootulp@gmail.com>
2022-04-26 04:29:00 +0200

    Clean up front-end

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,44 +1,15 @@
-import { useStarknetCall, useStarknetTransactionManager } from '@starknet-react/core'
+import { useStarknetCall } from '@starknet-react/core'
 import Head from 'next/head'
-import { useEffect, useMemo, useState } from 'react'
-import { toBN } from 'starknet/dist/utils/number'
-import { ConnectWallet } from '~/components/ConnectWallet'
-import { IncrementCounter } from '~/components/IncrementCounter'
-import { TransactionList } from '~/components/TransactionList'
-import { useCounterContract } from '~/hooks/counter'
+import { useMemo } from 'react'
 import { useOracleContract } from '~/hooks/oracle'
 import { bigNumberishArrayToDecimalStringArray } from 'starknet/dist/utils/number'
 import styles from '../styles/Home.module.css'
-
-import Card from '@mui/material/Card';
 import { Paper } from '@mui/material'
 import 'css-doodle'
-import { Html } from 'next/document'
 
 const Home = () => {
-
-  const { contract: counter } = useCounterContract()
   const { contract: oracleContract } = useOracleContract()
-  const [randomColor, setRandomColor] = useState()
-  const [randomGrid, setRandomGrid] = useState()
-
-
-  // boilerplate 
-  // const { data: counterResult } = useStarknetCall({
-  //   contract: counter,
-  //   method: 'counter',
-  //   args: [],
-  // })
-
-  // const counterValue = useMemo(() => {
-  //   if (counterResult && counterResult.length > 0) {
-  //     const value = toBN(counterResult[0])
-  //     return value.toString(10)
-  //   }
-  // }, [counterResult])
-
-  // get random value 
-  const { data: oracleResult, error } = useStarknetCall({
+  const { data: oracleResult } = useStarknetCall({
     contract: oracleContract,
     method: 'get_value',
     args: [
@@ -55,7 +26,6 @@ const Home = () => {
 
       const six = `0x${hex.substring(0, 6)}`
       console.log('six: ', six);
-      setRandomColor(six);
       return value[0];
     }
   }, [oracleResult])
@@ -64,11 +34,10 @@ const Home = () => {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Random Feed-ooooo-r</title>
+        <title>randomfeedooooor</title>
         <meta name="description" content="generates random seeds for starknet"></meta>
       </Head>
       <main className={styles.main}>
-
         <h1 className={styles.title}
           style={{
             font: 'roboto',
@@ -76,17 +45,8 @@ const Home = () => {
             color: "white"
           }}>
           <div>Welcome to</div>
-          <div>Random Feed-ooooo-r</div>
+          <div>randomfeedooooor</div>
         </h1>
-
-        {/* 
-        3 to 20 for @grid
-        left border color
-        top border color 
-        shape 3 to 5 random 
-        
-        S and L both from 40 to 100 
-      */}
       <css-doodle click-to-update>
         {`
       :doodle {
@@ -104,7 +64,6 @@ const Home = () => {
           `
       }
       </css-doodle>
-
         <div style={{
           position: "fixed",
           top: '40%',
@@ -112,50 +71,16 @@ const Home = () => {
           flexDirection: "column",
 
         }}>
-
-          <Paper elevation={3} style={{
-            marginBottom: "4%",
-            backgroundColor: "black"
-          }}>
-            <h2
-              style={{
-                paddingLeft: "4%",
-                color: "white"
-              }}
-            >Random Seed</h2>
-            <p style={{
-              paddingLeft: "4%",
-              paddingRight: "4%",
-              color: "white"
-            }}>{randomNumber}</p>
-
+          <Paper elevation={3} style={{marginBottom: "2em"}}>
+            <h2>Current random value</h2>
+            <p>{randomNumber}</p>
           </Paper>
-
-
-          <Paper elevation={3} style={{
-            marginBottom: "4%"
-          }}>
-            <h2 style={{
-              paddingLeft: "4%"
-            }}>Oracle Contract</h2>
-            <p>Address: {oracleContract?.address}</p>
-          </Paper>
-
           <Paper elevation={3}>
-            <h2 style={{
-              paddingLeft: "4%"
-            }}>Past Seeds</h2>
-            <TransactionList />
+            <h2>Oracle Contract</h2>
+            <p>{oracleContract?.address}</p>
           </Paper>
-          <div>
-
-          </div>
         </div>
-
-
-
       </main>
-
     </div>
   )
 }
